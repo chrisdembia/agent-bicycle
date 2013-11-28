f1ef19d7ee8b69c4d6ae627a4af8ceb1af5c90ef
bcam <brc.cam@gmail.com>
2013-11-27 17:57:05 -0800

    Many changes for implementing the goto task
    Added getter functions for heading angle state in environment.py
    
    Added subclass SARSALambda_LinFA_setAlpha  to learners.py that allows us
    to set the learning rate applied to the heading angle states.
    
    Implemented Randlov's goto task in tasks.py via subclass
    LinearFATileCoding3456GotoTask.
    
    goto_sarsalambda_linfa_replacetrace.py is the script used to execute the
    training.

--- randlov1998/environment.py
+++ randlov1998/environment.py
@@ -63,6 +63,15 @@ class Environment(pybrain.rl.environments.environment.Environment):
     def getYF(self):
         return self.sensors[6]
 
+    def getXB(self):
+        return self.sensors[7]
+
+    def getYB(self):
+        return self.sensors[8]
+
+    def getPSI(self):
+        return self.sensors[9]
+
     def get_xfhist(self):
         return self.xfhist
 
@@ -214,4 +223,3 @@ class Environment(pybrain.rl.environments.environment.Environment):
         self.yfhist = []
         self.xbhist = []
         self.ybhist = []
-
